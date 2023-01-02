acf6bc557450e0827662d86c51fedcf2eef13e4f
grsurgeon001 <grsurgeon001@gmail.com>
2023-01-03 08:47:39 +0900

    test branch1

--- js/bg-image.js
+++ js/bg-image.js
@@ -3,7 +3,25 @@ const body=document.querySelector("body");
 function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+const today = new Date();
+const dayCode = {
+    year : today.getFullYear(),
+    month : today.getMonth(),
+    date : today.getDate(),
+} 
 
-const randomNumber=rand(0,4);
+console.log(dayCode);
 
-body.style.backgroundImage=`url("img/${randomNumber}.jpg")`;
+const storedBgImg = {
+    storedId : dayCode,
+    imageUrl :
+}
+if (localStorage.getItem("today-bg")){
+    body.style.backgroundImage= ;
+} else {
+    const randomNumber=rand(0,4);
+    localStorage.setItem("today-bg",`url("img/${randomNumber}.jpg")`)
+    body.style.backgroundImage=`url("img/${randomNumber}.jpg")`;
+}
+
+
