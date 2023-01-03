fd5a28dc59757f804aba3a1948aa0fdcc4f7fccf
grsurgeon001 <grsurgeon001@gmail.com>
2023-01-03 22:04:48 +0900

    todos, advice, bg image, greetings, login, clock - completed

--- js/todos.js
+++ js/todos.js
@@ -16,8 +16,9 @@ function reloadTodo(){
     }
 }
 
-function paintTodo(new_todo){
+function paintTodo(new_todo,id){
     const li = document.createElement("li");
+    li.setAttribute("id",id);
     const span = document.createElement("span");
     span.innerText = new_todo;
     const button = document.createElement("button");
@@ -37,15 +38,21 @@ function saveNewTodo(event){
     }
     todolist.push(todos_item);
     localStorage.setItem("todos",JSON.stringify(todolist));
-    paintTodo(new_todo);
+    paintTodo(new_todo,todos_item._id);
 }
 
-function deleteToDo(){
+function deleteToDo(event){
+    const target_id = event.path[0].parentElement.id;
+    const target_li = document.getElementById(`${target_id}`);
+    todoList.removeChild(target_li);
+    todolist = todolist.filter((todo) => todo._id !== parseInt(target_id));
+    localStorage.setItem("todos",JSON.stringify(todolist));
+    
 
 }
 
 reloadTodo();
 todolist.forEach(element => {
-    paintTodo(element);
+    paintTodo(element.todo,element._id);
 });
 todosForm.addEventListener("submit",saveNewTodo);
