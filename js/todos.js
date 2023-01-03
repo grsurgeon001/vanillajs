e52fb259ee4813b0341ad125b2ed2fe6a8a474d7
grsurgeon001 <grsurgeon001@gmail.com>
2023-01-03 17:27:03 +0900

    minor change

--- js/todos.js
+++ js/todos.js
@@ -1,26 +1,51 @@
 const todosForm = document.querySelector(".todos-form");
 const todosInput = document.querySelector(".todos-input");
+const todoList= document.querySelector(".todo-list__ul");
 
 
 console.log(todosForm);
 
 let todolist = [];
-console.log(todolist);
 
 function reloadTodo(){
     const saved_todos = JSON.parse(localStorage.getItem("todos"));
-    saved_todos.forEach(element => {
+    if (saved_todos){
+        saved_todos.forEach(element => {
         todolist.push(element);
-    });
-    console.log("updated",todolist);
+        });
+    }
+}
+
+function paintTodo(new_todo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = new_todo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li);
 }
 
 function saveNewTodo(event){
     event.preventDefault();
     const new_todo = todosInput.value;
-    todolist.push(new_todo);
+    const todos_item = {
+        "_id" : Date.now(),
+        "todo" : new_todo,
+    }
+    todolist.push(todos_item);
     localStorage.setItem("todos",JSON.stringify(todolist));
+    paintTodo(new_todo);
+}
+
+function deleteToDo(){
+
 }
 
 reloadTodo();
+todolist.forEach(element => {
+    paintTodo(element);
+});
 todosForm.addEventListener("submit",saveNewTodo);
